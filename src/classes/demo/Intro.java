992f43829cbee460baf88b999885e58161cdeadd
Pisethsek <pisethsek9899@gmail.com>
2023-03-05 15:47:14 +0700

    class obj again

--- src/classes/demo/Intro.java
+++ src/classes/demo/Intro.java
@@ -1,2 +1,17 @@
-package classes.demo;public class Intro {
+package classes.demo;
+
+class People {
+    int id;
+    String name;
+    People(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+}
+public class Intro {
+    public static void main(String[] args) {
+        People p1 = new People(1001, "Lao");
+        System.out.println(p1.id);
+        System.out.println(p1.name);
+    }
 }

589ceaa8e6e52792353f6de79108e681f7392372
Pisethsek <pisethsek9899@gmail.com>
2023-03-07 16:11:06 +0700

    mvc patterns

---
+++ src/pattern/mvc/MVCPatternDemo.java
@@ -0,0 +1,28 @@
+package pattern.mvc;
+
+import pattern.mvc.controller.StudentController;
+import pattern.mvc.model.Student;
+import pattern.mvc.view.StudentView;
+
+public class MVCPatternDemo {
+    public static void main(String[] args) {
+        Student model = retriveStudentFromDatabase();
+
+        StudentView view = new StudentView();
+
+        StudentController controller = new StudentController(model, view);
+
+        controller.updateView();
+
+        controller.setStudentName("Hello");
+
+        controller.updateView();
+    }
+
+    private static Student retriveStudentFromDatabase(){
+        Student student = new Student();
+        student.setName("Robert");
+        student.setRollNo("10");
+        return student;
+    }
+}
